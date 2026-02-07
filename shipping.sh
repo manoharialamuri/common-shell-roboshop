07cda763816c938b90fdd47b13921c5f5ab88d90
manu <example@gmail.com>
2026-02-07 20:31:23 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -7,7 +7,7 @@ app_setup
 java_setup
 systemd_setup
 dnf install mysql -y  &>>$LOGS_FILE
-VALIDATE $? "Installing MySQL"
+validate $? "Installing MySQL"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
@@ -15,7 +15,7 @@ if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
-    VALIDATE $? "Loaded data into MySQL"
+    validate $? "Loaded data into MySQL"
 else
     echo -e "data is already loaded ... $Y SKIPPING $N"
 fi
